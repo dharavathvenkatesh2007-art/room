462bd1c40c651a6d8951dd428c84ef86be9f37d4
dharavathvenkatesh2007-art <dharavathvenkatesh2007@gmail.com>
2026-03-12 21:24:16 +0530

    Delete HostelProject directory

--- HostelProject/backend/models/Booking.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const bookingSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  hostel: { type: mongoose.Schema.Types.ObjectId, ref: "Hostel" },
-  date: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("Booking", bookingSchema);
